a807cd5f1ee9b961b53d27a5cbf9a5616ad919f1
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-09-22 14:41:27 +0600

    controller

--- src/main/java/com/ash/taskmanager/controller/test.java
+++ src/main/java/com/ash/taskmanager/controller/test.java
@@ -1,9 +1,11 @@
 package com.ash.taskmanager.controller;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableAutoConfiguration
 @RequestMapping(path = "/")
 public class test {
 
